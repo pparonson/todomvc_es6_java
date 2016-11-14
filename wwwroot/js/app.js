535b52bcb4c252fc3f2a9b6c8ac6b91c64059245
pparonson <preston.aronson@gmail.com>
2016-11-14 06:41:07 -0700

    implement click events master branch

--- wwwroot/js/app.js
+++ wwwroot/js/app.js
@@ -1,12 +1,8 @@
 $(document).ready(() => {
     console.log('READY');
-    views.displayTodos();
     views.toggleAll();
     views.addTodo();
-    views.editTodo();
-    views.deleteTodo();
-    views.toggleCompleted();
-    views.displayTodos();
+    views.setUpEventListeners();
 });
 const todoList = {
     todos: [],
@@ -41,7 +37,7 @@ const todoList = {
     },
     toggleCompleted(position) {
         // destructure array; obj
-        let { completed } = this.todos[position];
+        const { completed } = this.todos[position];
         this.todos[position].completed = !(completed);
         views.displayTodos();
     },
@@ -70,34 +66,22 @@ const views = {
             todoList.addTodo(addTodoTextInput.val());
             addTodoTextInput.val(null);
         });
+        // $('form').submit(() => {
+        //     const addTodoTextInput = $('#addTodoTextInput');
+        //     todoList.addTodo(addTodoTextInput.val());
+        //     console.log(addTodoTextInput.val());
+        //     addTodoTextInput.val(null);
+        // });
     },
-    editTodo() {
-        const editTodoButton = $('#editTodoButton');
-        $(editTodoButton).click(() => {
-            const editTodoPositionInput = $('#editTodoPositionInput');
-            const editTodoTextInput = $('#editTodoTextInput');
-            todoList.editTodo(editTodoPositionInput.val(),
-                editTodoTextInput.val());
-            editTodoPositionInput.val(null);
-            editTodoTextInput.val(null);
-        });
+    editTodo(position, todoText) {
+
+        todoList.editTodo(position, todoText);
     },
-    deleteTodo() {
-        const deleteTodoButton = $('#deleteTodoButton');
-        $(deleteTodoButton).click(() => {
-            const deleteTodoPositionInput = $('#deleteTodoPositionInput');
-            todoList.deleteTodo(deleteTodoPositionInput.val());
-            deleteTodoPositionInput.val(null);
-        });
+    deleteTodo(position) {
+        todoList.deleteTodo(position);
     },
-    toggleCompleted() {
-        const toggleCompletedButton = $('#toggleCompletedButton');
-        $(toggleCompletedButton).click(() => {
-            const toggleCompletedPositionInput =
-                $('#toggleCompletedPositionInput');
-            todoList.toggleCompleted(toggleCompletedPositionInput.val());
-            toggleCompletedPositionInput.val(null);
-        });
+    toggleCompleted(position) {
+        todoList.toggleCompleted(position);
     },
     displayTodos() {
         // There should be an li tag element for every todo item
@@ -105,7 +89,13 @@ const views = {
         todosUl.addClass('todosUl');
         $('.todosUl').html('');
         todoList.todos.forEach(todo => {
-            const todoLi = $("<li></li>");
+            // const divInputGroup = $('<div></div>');
+            // divInputGroup.addClass('input-group');
+
+            const todoLi = $('<li></li>');
+            todoLi.addClass('input-group');
+
+
             const { todoText, completed } = todo;
             $(todoLi).attr('id', todoList.todos.indexOf(todo));
             let todoTextWithCompletion = '';
@@ -114,16 +104,77 @@ const views = {
             } else {
                 todoTextWithCompletion = `( ) ${ todo.todoText }`;
             }
-            console.log(todoTextWithCompletion);
             $(todosUl).append(todoLi.text(todoTextWithCompletion));
+            // $(todosUl).append(todoLi);
+
+            $(todoLi).prepend(this.createEditTodoTextInput());
+            $(todoLi).prepend(this.createToggleCompletedButton());
+            $(todoLi).append(this.createEditTodoButton());
             $(todoLi).append(this.createDeleteTodoButton());
         });
     },
+    createToggleCompletedButton() {
+        const toggleCompletedButton = $('<button></button>');
+        toggleCompletedButton.addClass(
+            "toggleCompletedButton btn btn-info input-group-button")
+            .text('Toggle');
+
+        // const toggleCompletedSpan = $('<span></span>');
+        // toggleCompletedSpan.hasClass('input-group-button');
+        // const toggleCompletedButton = $('<button></button>');
+        // toggleCompletedButton.addClass("toggleCompletedButton btn btn-info")
+        //     .text('Toggle');
+        //
+        // const toggleCompleted = $(toggleCompletedSpan)
+        //     .append(toggleCompletedButton);
+        return toggleCompletedButton;
+        // return toggleCompleted;
+    },
     createDeleteTodoButton() {
         const deleteTodoButton = $('<button></button>');
-        deleteTodoButton.addClass('deleteTodoButton btn btn-default')
+        deleteTodoButton.addClass(
+            "deleteTodoButton btn btn-danger")
             .text('Delete');
         return deleteTodoButton;
-    }
+    },
+    createEditTodoButton() {
+        const editTodoButton = $('<button></button>');
+        editTodoButton.addClass(
+            "editTodoButton btn btn-warning").text('Edit');
+        return editTodoButton;
+    },
+    createEditTodoTextInput() {
+        const editTodoTextInput = $('<input type="text">');
+        editTodoTextInput.addClass("editTodoTextInput form-control");
+        return editTodoTextInput;
+    },
+    setUpEventListeners() {
+        const todoUl = $('ul');
+        todoUl.click(event => {
+            // use destructuring to obtain target and id prop of event obj
+            const { target } = event;
+            const { target: { parentNode: parentNode} } = event;
+            const { target: {parentNode: {id: id } } } = event;
 
+            if ($(target).hasClass('toggleCompletedButton')) {
+                console.log(target);
+                this.toggleCompleted(parseInt(id));
+            } else if ($(target).hasClass('deleteTodoButton')) {
+                console.log(id);
+                this.deleteTodo(parseInt(id));
+            } else if ($(target).hasClass('editTodoTextInput')) {
+                $(parentNode).append(this.createEditTodoTextInput());
+
+            } else if ($(target).hasClass('editTodoButton')) {
+                // need a positon and todoText to pass to this.editTodo();
+
+
+                // this.editTodo(parseInt(id), editTodoTextInput);
+
+            } else {
+                console.log(event);
+                console.log(event.target);
+            }
+        });
+    }
 }; // end: views
